e38bb552e6e7c04c1c77870cc393b81b6c683808
johanjs0812 <tanpv2023@gmail.com>
2024-12-16 20:23:49 +0700

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>
+        <link rel="icon" type="image/png" href="/imgs/logo.png" />
         <title>Fintown</title>
       </head>
       <body className="font-inter custom-scrollbar">
